dc6cd3bd2adfe7e74934235f8005e0eb871498bc
Maria T. Andersen <mteilmann93@gmail.com>
2021-12-16 09:45:25 +0100

    create a button component

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="button">
+    <button>{{buttonText}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    buttonText: String,
+  }
+}
+</script>
+
+<style scoped>
+</style>
