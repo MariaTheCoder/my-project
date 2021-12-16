e1187a732af742921bd0326f529492eb9b7c3826
Maria T. Andersen <mteilmann93@gmail.com>
2021-12-16 09:46:48 +0100

    edit component headline

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <ContentDisplayer headline="Maria"/>
+  <ContentDisplayer headline="HEADLINE"/>
 </template>
 
 <script>
@@ -22,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
