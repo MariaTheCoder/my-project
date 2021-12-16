557d92ed8807d42bfeda62d93eece1e0fc7f8c6e
Maria T. Andersen <mteilmann93@gmail.com>
2021-12-16 09:46:20 +0100

    import button component and use a method upon click on said button

--- src/components/ContentDisplayer.vue
+++ src/components/ContentDisplayer.vue
@@ -1,21 +1,34 @@
 <template>
   <div class="Displayer">
-    <ul>Hello {{headline}}</ul>
+    <h3>{{headline}}</h3>
+    <Button @click="getUsers" buttonText="SOME TEXT"/>
   </div>
 </template>
 
 <script>
+import Button from './Button.vue'
+
 export default {
   name: 'ConcentDisplayer',
   props: {
     headline: String,
+  },
+  components: {
+    Button,
+  },
+  methods: {
+    getUsers() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => console.log(json))
+    }
   }
 }
 </script>
 
 <style scoped>
-  ul {
+  h3 {
     font-weight: bolder;
-    font-size: 100px;
+    font-size: 50px;
   }
 </style>
