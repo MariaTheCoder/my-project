2f14d2d35b0413ab33a3cd25cac3cc1653de0c08
Maria T. Andersen <mteilmann93@gmail.com>
2021-12-16 23:29:35 +0100

    create user component

---
+++ src/components/Users.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="Users">
+    <p>{{nameOfUser}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Users',
+  props: {
+    nameOfUser: String,
+  },
+}
+</script>
+
+<style scoped>
+p {
+  font-weight: bold;
+}
+</style>
